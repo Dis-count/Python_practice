cbd8907efc29062b9dbdda614e9ab9ec1d490f09
Discount <33273755+Dis-count@users.noreply.github.com>
2020-01-03 20:50:00 +0800

    LeetCode

---
+++ LeetCode/1Sum.py
@@ -0,0 +1,8 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dict = {}
+        for i in range(len(nums)):
+            if target-nums[i] not in dict:
+                dict[nums[i]]=i
+            else:
+                return [dict[target-nums[i]],i]a
