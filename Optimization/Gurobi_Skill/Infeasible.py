545258c05d0ca4fb317a008641c5af87ee96903c
Discount <33273755+Dis-count@users.noreply.github.com>
2020-06-08 11:13:07 +0800

    Update

--- Optimization/Gurobi_Skill/Infeasible.py
+++
@@ -1,8 +0,0 @@
-model = Model()
-vars = model.getVars()
-constrs = model.getConstrs()
-
-for i in range (len(constrs)):
-    print (constrs[i].getAttr(GRB.Attr.IISConstr))
-for i in range(len(solution)):
-    print (solution[i].getAttr(GRB.Attr.IISLB), "  ", vars[i].getAttr(GRB.Attr.IISUB))
