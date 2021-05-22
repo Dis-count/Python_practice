54ca0f53b210ef511df13a96aa54483eb8c5529e
Discount <492193947@qq.com>
2021-05-22 18:31:02 +0800

    5-22

--- practice/LeetCode/EverydayPrac/119.py
+++ practice/LeetCode/EverydayPrac/119.py
@@ -19,3 +19,44 @@
 # Input words contain only lowercase letters.
 # Follow up:
 # Try to solve it in O(n log k) time and O(n) extra space.
+
+# 思路
+# 可以看到，本题有两个排序关键字：
+#
+# 词频 倒序排列
+# 若词频相同，按字母顺序排序 正序排列
+# 接下来我们看看如何在python里一次对两个关键字进行排序
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+
+        hash = collections.Counter(words)
+        res = sorted(hash, key=lambda word:(-hash[word], word))
+        return res[:k]
+
+# 用小顶堆维护，统计词频，用类封装词和词频，重写运算符
+
+import heapq, collections
+
+class Word:
+    def __init__(self, word, fre):
+        self.word = word
+        self.fre = fre
+    def __lt__(self, other):
+        if self.fre != other.fre:
+            return self.fre < other.fre
+        return self.word > other.word
+
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+
+        cnt = collections.Counter(words)
+        heap = []
+
+        for word, fre in cnt.items():
+            heapq.heappush(heap, Word(word, fre))
+            if len(heap) > k:
+                heapq.heappop(heap)
+
+        heap.sort(reverse=True)
+        return [x.word for x in heap]
---
+++ practice/LeetCode/EverydayPrac/121.py
@@ -0,0 +1,43 @@
+# 1035. Uncrossed Lines
+# We write the integers of nums1 and nums2 (in the order they are given) on two separate horizontal lines.
+#
+# Now, we may draw connecting lines: a straight line connecting two numbers nums1[i] and nums2[j] such that:
+#
+# nums1[i] == nums2[j];
+# The line we draw does not intersect any other connecting (non-horizontal) line.
+# Note that a connecting lines cannot intersect even at the endpoints: each number can only belong to one connecting line.
+#
+# Return the maximum number of connecting lines we can draw in this way.
+#
+#
+# Example 1:
+#
+# Input: nums1 = [1,4,2], nums2 = [1,2,4]
+# Output: 2
+# Explanation: We can draw 2 uncrossed lines as in the diagram.
+# We cannot draw 3 uncrossed lines, because the line from nums1[1]=4 to nums2[2]=4 will intersect the line from nums1[2]=2 to nums2[1]=2.
+#
+# Example 2:
+#
+# Input: nums1 = [2,5,1,2,5], nums2 = [10,5,2,1,5,2]
+# Output: 3
+#
+# Example 3:
+#
+# Input: nums1 = [1,3,7,1,7,5], nums2 = [1,9,2,5,1]
+# Output: 2
+
+
+class Solution:
+    def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
+        m, n = len(nums1), len(nums2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i, num1 in enumerate(nums1):
+            for j, num2 in enumerate(nums2):
+                if num1 == num2:
+                    dp[i + 1][j + 1] = dp[i][j] + 1
+                else:
+                    dp[i + 1][j + 1] = max(dp[i][j + 1], dp[i + 1][j])
+
+        return dp[m][n]
